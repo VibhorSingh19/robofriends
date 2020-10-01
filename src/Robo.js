4e37fb14478e73d7b47504bc646a4eb66ebc09f3
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-01 13:09:55 +0530

    update

---
+++ src/Robo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Robo() {
+  return (
+    <div>
+    <header>
+     <img src="https://robohash.org/ddd" Alt="robo"></img>
+     <h1>Robo1</h1>
+     <p>robo1@gmail.com</p>
+    </header>
+    </div>
+
+    );
+}
+
+export default Robo;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Robo from './Robo';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <Robo />
   </React.StrictMode>,
   document.getElementById('root')
 );
