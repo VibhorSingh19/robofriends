d4ca157b1d67460f84c713486c7b3e2ec126401e
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-03 11:13:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Robo from './Robo';
+//import Robo from './Robo';
 //import Login from './Login';
 
 import 'tachyons';
